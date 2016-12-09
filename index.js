88b50318e069dd84ecb807ad39cfd5c13c561fa1
Rob Loach <robloach@gmail.com>
2016-12-09 13:27:45 -0500

    Add xo as coding style (#37)

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
-'use strict';
+'use strict'
 
-var Promise = require('promise');
-var AnalyzeCSS = require('analyze-css');
+var Promise = require('promise')
+var AnalyzeCSS = require('analyze-css')
 
-exports.name = 'analyze-css';
-exports.outputFormat = 'json';
+exports.name = 'analyze-css'
+exports.outputFormat = 'json'
 
 exports.renderAsync = function (str, options) {
   return new Promise(function (resolve, reject) {
-    options = options && typeof options === 'object' ? options : {};
+    options = options && typeof options === 'object' ? options : {}
+    // eslint-disable-next-line no-new
     new AnalyzeCSS(str, options, function (err, results) {
       if (err) {
-        return reject(err);
+        return reject(err)
       }
-      var output = JSON.stringify(results, null, 2);
-      resolve(output);
-    });
-  });
-};
+      var output = JSON.stringify(results, null, 2)
+      resolve(output)
+    })
+  })
+}
